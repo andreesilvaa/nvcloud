33d6b447085b5217095afdd4b6efc6a61679e351
André Esteves Silva <jmyq7tzhfc@privaterelay.appleid.com>
2026-06-08 15:32:01 +0100

    .

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'newvision/nvcloud',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d6d154e0561b0296163d60b166ce814e99c6701',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'newvision/nvcloud' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d6d154e0561b0296163d60b166ce814e99c6701',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
